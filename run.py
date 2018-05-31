c85031c5fe286579817833c90de1c03ba251b1a0
ssewilliam <21138053+ssewilliam@users.noreply.github.com>
2018-05-31 12:49:04 +0300

    Update run.py and add create request endpoint route

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
-# list data storage with fake data
+# list data storage with fake data that would be used for get requests
 request_data_list = [
     {
         'request_id':'1',
@@ -25,10 +25,13 @@ request_data_list = [
 
 @app.route('/api_v_1/users/requests',methods=['GET'])
 def get_all_requests():
-    # check if the data storage has some data
+    """
+        This endpoint returns all maintance request tickets
+    """    
+    # 1. check if the data storage has some data
     if len(request_data_list) < 1:
         abort(404)
-    # return all data stored
+    # 2. return all data stored
     return jsonify({
         'status':'OK',
         'requests':request_data_list
@@ -36,17 +39,49 @@ def get_all_requests():
 
 @app.route('/api_v_1/users/requests/<requestId>',methods=['GET'])
 def get_single_request(requestId):
-    # fetch data from storage and check if the id passed matches storage id
+    """
+        This endpoint returns a single maintance request ticket based on the id parsed
+    """
+    # 1. fetch data from storage and check if the id passed matches storage id
     request_data = [
             request_data for request_data in request_data_list
             if request_data['request_id'] == requestId
         ]
-    # check if there is data already stored        
+    # 2. check if there is data already stored        
     if len(request_data) == 0:
         abort(404)
-    return jsonify({
+    return jsonify({ # 3. return the found request id details
         'request_data':request_data[0]
     })
 
+@app.route('/api_v_1/users/requests' ,methods= ['POST'])
+def create_request():
+    """
+        This endpoint creates a maintance request ticket
+    """
+    # 1. force getting json data from a request
+    request_data = request.get_json(force=True)
+    # 2. validate the data checkking if its of examples of strings
+    if isinstance(request_data['request_type'], str) and isinstance(request_data['request_title'], str):
+        
+        # 3. store the data
+        request_last_id = len(request_data_list) #  count how many maintance requests are already saved
+        request_last_id += 1 
+        request_datas = {
+            'request_id':request_last_id,
+            'request_title':request_data['request_title'],
+            'request_type': request_data['request_type']
+        }
+        # create an instance of request and immitate the saving of data
+        request_data_list.append(request_datas)  #  Save request in the list
+        
+        return jsonify({
+            'results':request_datas
+        }), 201
+        
+    return jsonify({
+        'status': 'FAIL',
+        'message': 'Failed to create a request. Invalid data',
+    }), 400
 if __name__ == "__main__":
     app.run(debug=True)
